ea45ec3798e66e5f44c0ad8ebdf3756631a0d0b9
anthony-murè <tony.mure99@gmail.com>
2025-07-12 20:38:10 +0200

    [add]event-click-button/messege-final-result

--- simon_says_dom/js/functions.js
+++ simon_says_dom/js/functions.js
@@ -11,15 +11,15 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 const countdown = document.getElementById('countdown');
 const numbersList = document.getElementById('numbers-list');
 const formAnswers = document.getElementById('form-answers');
-const inputNumbers =document.querySelectorAll('form-control');
+const inputNumbers = document.querySelectorAll('.form-control');
 const button = document.getElementById('btn');
 
-/*attraverso la numberslist genero in modo randomico 5 numeri compresi tra 1 e 50 
-/*da quando i numeri random appaiono in pagina deve partire un countdown da 30 secondi
-/*passati 10 secondi i numeri random devono sparire 
-/al posto dei numeri random devono apparire 5 imput per far inserire all'utente i numeri random visti
-/*creo l'evento al click del pulsante per confrontare i numeri random con quelli inseriti
-/*mostro i risultati del confronto inserendo il numero di numeri indovinato e mostrando quali numeri ha indovinato l'utente*/
+//attraverso la numberslist genero in modo randomico 5 numeri compresi tra 1 e 50 
+//da quando i numeri random appaiono in pagina deve partire un countdown da 30 secondi
+//passati 10 secondi i numeri random devono sparire 
+//al posto dei numeri random devono apparire 5 imput per far inserire all'utente i numeri random visti
+//creo l'evento al click del pulsante per confrontare i numeri random con quelli inseriti
+//mostro i risultati del confronto inserendo il numero di numeri indovinato e mostrando quali numeri ha indovinato l'utente
 
 //genero 5 numeri random
 const numberRandom = [ ];
@@ -33,7 +33,7 @@ for ( let i = 0; i < 5; i++){
 }
 console.log(numberRandom);
 
-document.getElementById('numbers-list').innerHTML = numberRandom;
+document.getElementById('numbers-list').innerHTML = numberRandom.join(' ');
 
 //faccio partire un countdown di 30s quando i numeri random appaiono in pagina
 
@@ -57,3 +57,31 @@ const timer = setInterval(() =>{
 
 }, 1000);
 
+//creo l'evento al click del pulsante per confrontare i numeri random con quelli inseriti
+//confrontare numbersRandom con inputnumbers
+//mostro i risultati del confronto inserendo il numero di numeri indovinato e mostrando quali numeri ha indovinato l'utente
+
+
+const message = document.getElementById('message');
+const numberFound = [];
+
+
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+    
+  const userNumbers = Array.from(inputNumbers).map(input => parseInt(input.value));
+  numberFound.length = 0;
+
+    for(let i = 0; i < numberRandom.length; i++){
+        
+      if(userNumbers.includes(numberRandom[i])){
+         numberFound.push(numberRandom[i]);
+      }
+      
+    }
+    
+  
+   message.innerHTML = `Hai indovinato ${numberFound.length} numeri!:( ${numberFound.join(' ')})`;
+
+   });
+  
